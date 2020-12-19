d6ec849d2757759a4c273bb72a51fae31c8d57ac
taiga <taiga.similarities@gmail.com>
2020-12-19 10:25:48 +0900

    add prettier

--- src/public/Register.tsx
+++ src/public/Register.tsx
@@ -31,7 +31,7 @@ class Register extends Component {
 
   render() {
     if (this.state.redirect) {
-      return <Redirect to={'/login'} />;
+      return <Redirect to={'/login/'} />;
     }
 
     return (
