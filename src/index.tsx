c849c6602a498b596512896da5e0f79647c058e3
taiga <taiga.similarities@gmail.com>
2020-12-23 11:44:05 +0900

    add redux dependencies and index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+import { Provider } from 'react-redux';
+import configureStore from './redux/configureStore';
+
+axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.headers.Authorization = `Bearer ${localStorage.getItem(
+  'token'
+)}`;
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
